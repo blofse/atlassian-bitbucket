8c295beaf4b0a10a6ad57eca5d1e3327ddff90a6
blofse <blofse@users.noreply.github.com>
2017-05-22 17:31:48 +0100

    Updating README, adding additional optional services, renaming the image/volume/networks, adding network rather than a link

--- optional/copy_install_and_start_as_service.sh
+++ optional/copy_install_and_start_as_service.sh
@@ -2,16 +2,16 @@
 
 echo Stopping existing container
 docker stop atlassian-bitbucket
-docker stop atlassian-bitbucket-mysql
+docker stop atlassian-bitbucket-database
 
 echo Copying and running service
-yes | cp docker-atlassian-bitbucket-mysql.service /etc/systemd/system/.
-yes | cp docker-atlassian-bitbucket.service /etc/systemd/system/.
+yes | cp optional/docker-atlassian-bitbucket-database.service /etc/systemd/system/.
+yes | cp optional/docker-atlassian-bitbucket.service /etc/systemd/system/.
 systemctl daemon-reload
 
-systemctl enable docker-atlassian-bitbucket-mysql
+systemctl enable docker-atlassian-bitbucket-database
 systemctl enable docker-atlassian-bitbucket
 
-systemctl start docker-atlassian-bitbucket-mysql
+systemctl start docker-atlassian-bitbucket-database
 systemctl start docker-atlassian-bitbucket
 echo Done!
